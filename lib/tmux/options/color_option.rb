4b11846e8ebdfe98dabb152ec59d35d6a9c31fe5
Dominik Honnef <dominikh@fork-bomb.org>
2010-07-23 11:27:09 +0200

    reject invalid color names in ColorOption#to_tmux

--- lib/tmux/options/color_option.rb
+++ lib/tmux/options/color_option.rb
@@ -1,10 +1,21 @@
+# -*- coding: utf-8 -*-
 require "tmux/options/symbol_option"
 module Tmux
   module Options
-    # @todo maintain list of valid colors
     # @api private
     # @see Option
     class ColorOption < SymbolOption
+      # @param [Symbol<:black, :red, :green, :yellow, :blue, :magenta, :cyan, :white, :colour0 – colour255, :default>] value
+      # @return (see Option.to_tmux)
+      # @see Option.to_tmux
+      # @api private
+      def to_tmux(value)
+        if ![:black, :red, :green, :yellow, :blue, :magenta, :cyan, :white, :default].include?(value) &&
+            value !~ /^colour\d+$/
+          raise ArgumentError
+        end
+        super
+      end
     end
   end
 end
